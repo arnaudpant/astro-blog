443b6fa1a41a2850334016196d9e4289f442d25a
macbook kal <arnaud.pantenier@gmail.com>
2025-04-14 22:34:01 +0200

    ADD Mode server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  output: 'server',
+  integrations: [react(), mdx()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
