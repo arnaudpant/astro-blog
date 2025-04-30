e4f81979ce6f79174810ed52de1cb51111161d8e
macbook kal <arnaud.pantenier@gmail.com>
2025-04-30 22:06:27 +0200

    ADD fichier const + titles optimisé SEO

---
+++ src/lib/constantes.ts
@@ -0,0 +1,8 @@
+export const SITE_TITLE = "Développement Web avec Astro & React.js | Blog & Ressources"
+export const SITE_DESCRIPTION = "Blog dédié au développement web avec Astro et React.js. Découvre des tutoriels pratiques, des astuces de code et des projets concrets pour progresser rapidement."
+export const ABOUT_TITLE = "À propos — Développeur Web React.js qui apprend Astro"
+export const ABOUT_DESCRIPTION = "Je suis développeur web spécialisé en React.js et j'explore Astro pour enrichir mes compétences. Découvre mon parcours, mes projets et mes objectifs avec ce Framework"
+export const BLOG_TITLE = "Blog — Articles sur Astro, React.js & Développement Web"
+export const BLOG_DESCRIPTION = "Lis mes articles de blog sur le développement web avec Astro et React.js. Tutoriels, astuces, projets pratiques et retours d'expérience d’un développeur en apprentissage continu."
+export const HOME_ARTICLES_LIMIT = 3
+export const ARTICLES_PER_PAGE = 10
